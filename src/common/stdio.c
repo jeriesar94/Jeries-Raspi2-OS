40f9c4b77c2d088e777505753451b86870df636e
Jeries Abedrabbo <jabedrabbo@asaltech.com>
2018-08-24 01:58:14 +0300

    Added common src, and header files.

---
+++ src/common/stdio.c
@@ -0,0 +1,17 @@
+#include <common/stdio.h>
+
+char getc(void* src){
+	
+}
+
+void putc(void* dst, char c){
+	
+}
+
+char* gets(void* src){
+	
+}
+
+void puts(void* dst, char* s){
+	
+}
---
+++ src/common/stdlib.c
@@ -0,0 +1,13 @@
+#include <common/stdlib.c>
+
+void memcpy(void* src, void* dst, int size){
+	
+}
+
+void bzero(void* dst, int size){
+	
+}
+
+char itoa(int val){
+	
+}
